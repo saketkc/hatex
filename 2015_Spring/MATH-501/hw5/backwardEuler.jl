f9a1855edadf04747464c2a26c9679286ddfba21
Saket Choudhary <saketkc@gmail.com>
2015-04-28 09:56:44 -0700

    Dump more

--- 2015_Spring/MATH-501/hw5/backwardEuler.jl
+++ 2015_Spring/MATH-501/hw5/backwardEuler.jl
@@ -1,3 +1,4 @@
+
 function func(t,x)
     return exp(2*t)*x^2
 end
@@ -13,11 +14,6 @@ function fixedpointFunc(tn, yn)
     # x_{n+1} - x_n -hfunc(t_{n+1}, x_{n+1}) =0
 end
 
-N = 10000
-h = 1/N
-time = linrange(0,1,N)
-tolerance = 10e-6
-maxSteps = 100
 
 function newton(a, b, index, previous)
     steps = 0
@@ -34,8 +30,17 @@ function newton(a, b, index, previous)
         end
         steps = steps+1
         if steps>100
+            if index==N
+                println("newton steps: ", steps)
+            end
             return mid
         end
+    if index==N
+        println("newton steps: ", steps)
+    end
+    end
+    if index==N
+        println("newton steps: ", steps)
     end
     return mid
 end
@@ -54,11 +59,14 @@ function fixedpoint(x0, index)
         x1=x2temp;
         k=k+1;
         if (abs(fixedpointFunc(time[index],x2)-x2)<tolerance)
-            shouldReturn=1;
-        end
-        if shouldReturn==1
+            if index==N
+                println("fp steps: ", k)
+            end
             return x2
         end
+    end
+    if index==N
+        println("fp steps: ", k)
     end
     return x2
 end
@@ -67,21 +75,38 @@ function exactsolution(index)
     return 1/(10.5-exp(2*time[index]))
 end
 
-newtonOut  = zeros(N)
-fixedpointOut = zeros(N)
-exactOut = zeros(N)
-newtonOut[1] = 0.1
-fixedpointOut[1] = 0.1
-exactOut[1] = exactsolution(1)
-### Implicit backward euler
-## Newton
+function main()
+    newtonOut  = zeros(N)
+    fixedpointOut = zeros(N)
+    exactOut = zeros(N)
+    newtonOut[1] = 0.1
+    fixedpointOut[1] = 0.1
+    exactOut[1] = exactsolution(1)
+    ### Implicit backward euler
+    ## Newton
 
 
-for i=2:N
-    println("Running", " ", i)
-    newtonOut[i] = newton(newtonOut[i-1], 1, i, newtonOut[i-1])
-    fixedpointOut[i] = fixedpoint(fixedpointOut[i-1], i)
-    exactOut[i] = exactsolution(i)
+    for i=2:N
+        #println("Running", " ", i)
+        newtonOut[i] = newton(newtonOut[i-1], exactsolution(i)+10*tolerance, i, newtonOut[i-1])
+        fixedpointOut[i] = fixedpoint(fixedpointOut[i-1], i)
+        exactOut[i] = exactsolution(i)
+        #println(newtonOut[i], " ", fixedpointOut[i], " ", exactOut[i])
 
-    println(newtonOut[i], " ", fixedpointOut[i], " ", exactOut[i])
+    end
+    #println(2*newtonOut[N], " ", fixedpointOut[N], " ", exactOut[N])
+    #println(exactOut[N]-2*newtonOut[N], " ", exactOut[N]-fixedpointOut[N])#, " ", exactOut[N])
+end
+
+x = [4,8,16,32,64, 128]
+N=0
+h=0
+tolerance = 10e-6
+maxSteps = 100
+for i=1:6
+    global N = x[i]
+    global h = 1/N
+    global time = linrange(0,1,N)
+
+    main()
 end
