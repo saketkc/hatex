e6c8c4d51ac5e493264f39c47676498c8ca995aa
Saket Choudhary <saketkc@gmail.com>
2015-06-26 13:52:43 -0700

    dump assign 1

---
+++ 2015_Summer/CSCI-585/Assignments/Assignment1/sql_data/projone.sql
@@ -0,0 +1,63 @@
+use DATABASE golf_db;
+/*
+Query 1
+*/
+SELECT count(distinct ID) from golfer;
+/*
++--------------------+
+| count(distinct ID) |
++--------------------+
+|                 22 |
++--------------------+
+1 row in set (0.02 sec)
+*/
+
+
+/*
+Query 2
+*/
+
+SELECT course_name FROM tee WHERE tee.yardage > 6680;
+/*
++-------------+
+| course_name |
++-------------+
+| Encino |
+| Rancho |
+| Wilson |
+| Wilson |
++-------------+
+*/
+
+/*
+Query 2
+*/
+
+SELECT distinct(golferID) FROM round WHERE round.course = 'WILSON';
+/*
++----------+
+| golferID |
++----------+
+| 1317 |
+| 1588 |
+| 2101 |
+| 2174 |
+| 2454 |
+| 3161 |
+| 4108 |
+| 4122 |
+| 4185 |
+| 4649 |
+| 4891 |
+| 5008 |
+| 6418 |
+| 7451 |
+| 8909 |
+| 8994 |
+| 9352 |
+| 9466 |
+| 9672 |
++----------+
+*/
+
+
