5a9073d67c487d0aefbf5747063e865397d31c17
Saket Choudhary <saketkc@gmail.com>
2015-07-01 11:30:43 -0700

    Finish project2. One of the times when you finish it way before the deadline!

--- 2015_Summer/CSCI-585/Assignments/Assignment2/assignment2_sql/projtwo.sql
+++ 2015_Summer/CSCI-585/Assignments/Assignment2/assignment2_sql/projtwo.sql
@@ -1,6 +1,9 @@
-/*Query 1
+use golf_db;
+/*
+Query 1
 */
 SELECT golfer.name FROM golfer where ID IN (SELECT golferID FROM round WHERE  round.score>80);
+/*
 +-----------+
 | name      |
 +-----------+
@@ -27,11 +30,13 @@ SELECT golfer.name FROM golfer where ID IN (SELECT golferID FROM round WHERE  ro
 | Jack      |
 | Lucas     |
 +-----------+
-
-
-/*Query 2
 */
-SELECT COUNT(golfer.name) FROM golfer WHERE ID NOT IN (SELECT golferID FROM round WHERE EXTRACT(YEAR FROM round.day)=2015);                          
+
+/*
+Query 2
+*/
+SELECT COUNT(golfer.name) FROM golfer WHERE ID NOT IN (SELECT golferID FROM round WHERE EXTRACT(YEAR FROM round.day)=2015);
+/*
 +-----------+
 | name      |
 +-----------+
@@ -58,12 +63,14 @@ SELECT COUNT(golfer.name) FROM golfer WHERE ID NOT IN (SELECT golferID FROM roun
 | Jack      |
 | Lucas     |
 +-----------+
-
-/*Query 3
 */
 
+/*
+Query 3
+*/
 
 SELECT golfer.name, COUNT(round.day) as '#rounds' FROM round INNER JOIN golfer ON golfer.ID=round.golferID GROUP BY golfer.name ;
+/*
 +-----------+---------+
 | name      | #rounds |
 +-----------+---------+
@@ -90,11 +97,13 @@ SELECT golfer.name, COUNT(round.day) as '#rounds' FROM round INNER JOIN golfer O
 | Ryan      |       6 |
 | William   |       5 |
 +-----------+---------+
+*/
 
-
-/*Query 4
+/*
+Query 4
 */
 SELECT golfer.name, COUNT(round.day) as '#rounds' FROM round INNER JOIN golfer ON golfer.ID=round.golferID WHERE EXTRACT(YEAR FROM round.day)=2013 GROUP BY golfer.name ;
+/*
 +-----------+---------+
 | name      | #rounds |
 +-----------+---------+
@@ -119,22 +128,21 @@ SELECT golfer.name, COUNT(round.day) as '#rounds' FROM round INNER JOIN golfer O
 | Ryan      |       3 |
 | William   |       1 |
 +-----------+---------+
-
-/* Query 5
 */
-mysql> CREATE VIEW differential AS SELECT (round.score-tee.course_rating)*113/tee.slope_rating FROM round INNER JOIN tee ON round.tee = tee.name;
 
-CREATE VIEW differential AS SELECT (round.score-tee.course_rating)*113/tee.slope_rating FROM round INNER JOIN tee WHERE round.tee = tee.name AND tee.course_name=round.course;
-
-CREATE VIEW test AS SELECT * , (round.score-tee.course_rating)*113/tee.slope_rating FROM round INNER JOIN tee WHERE round.tee = tee.name AND tee.course_name=round.course;
+/*
+Query 5
+*/
 CREATE VIEW differential AS SELECT * , (round.score-tee.course_rating)*113/tee.slope_rating AS differential FROM round INNER JOIN tee WHERE round.tee = tee.name AND tee.course_name=round.course;
 
 
 
-/(Query 6
+/*
+Query 6
 */
 
-SELECT DISTINCT(golfer.name), differential.differential FROM differential  INNER JOIN golfer WHERE golfer.ID=differential.golferID GROUP BY golfer.name ORDER BY differential.differential ASC LIMIT 3;                                                                                            
+SELECT DISTINCT(golfer.name), differential.differential FROM differential  INNER JOIN golfer WHERE golfer.ID=differential.golferID GROUP BY golfer.name ORDER BY differential.differential ASC LIMIT 3;
+/*
 +--------+--------------+
 | name   | differential |
 +--------+--------------+
@@ -142,11 +150,17 @@ SELECT DISTINCT(golfer.name), differential.differential FROM differential  INNER
 | Carter |     12.84091 |
 | Caleb  |     13.03063 |
 +--------+--------------+
+*/
 
+/*
+Query 7
+*/
 
- SELECT DISTINCT(golfer.name), differential.differential FROM differential  INNER JOIN golfer WHERE golfer.ID=differential.golferID AND differential.course='Harding' ORDER BY differential.differential ASC LIMIT 1;
+SELECT DISTINCT(golfer.name), differential.differential FROM differential  INNER JOIN golfer WHERE golfer.ID=differential.golferID AND differential.course='Harding' ORDER BY differential.differential ASC LIMIT 1;
+/*
 +------+--------------+
 | name | differential |
 +------+--------------+
 | Ryan |      5.32314 |
 +------+--------------+
+*/
