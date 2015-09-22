79bdd9882c4f87254bda6b512fc8c566aa12210c
Saket Choudhary <saketkc@gmail.com>
2015-09-22 05:08:33 -0700

    naive bayes

--- 2015_Fall/CSCI-567/Homework1/data/programming/naive_bayes.m
+++ 2015_Fall/CSCI-567/Homework1/data/programming/naive_bayes.m
@@ -1,15 +1,62 @@
 function [new_accu, train_accu] = naive_bayes(train_data, train_label, new_data, new_label)
-% naive bayes classifier
-% Input:
-%  train_data: N*D matrix, each row as a sample and each column as a
-%  feature
-%  train_label: N*1 vector, each row as a label
-%  new_data: M*D matrix, each row as a sample and each column as a
-%  feature
-%  new_label: M*1 vector, each row as a label
-%
-% Output:
-%  new_accu: accuracy of classifying new_data
-%  train_accu: accuracy of classifying train_data 
-%
-% CSCI 567: Machine Learning, Fall 2015, Homework 1
+train_data_size = size(train_data,1);
+test_data_size = size(new_data,1);
+
+uniquelabels = unique(train_label);
+uniquetestlabels = unique(new_label);
+alluniquelabels = union(uniquelabels, uniquetestlabels);
+priors = hist(train_label, alluniquelabels);
+priors = priors/sum(priors); %It should be a probability
+
+% We use a gaussian kernel for estimating the likelihood,
+% its parameters being the mean and standard deviation of that class.
+
+averages = ones(length(alluniquelabels), size(train_data,2));
+stddevs = ones(length(alluniquelabels), size(train_data,2));
+test_label = [];
+
+for j=1:length(alluniquelabels)        
+    averages(j,:) = mean(train_data(train_label == alluniquelabels(j),:));
+    stddevs(j,:) = std(train_data(train_label == alluniquelabels(j),:));
+end
+stddevs(stddevs(:,:) == 0) = 1;%;sqrt((train_data_size-1)/12);
+
+
+for j=1:test_data_size
+   for i=1:length(alluniquelabels)
+       prior = priors(i);
+       likelihood = pdf('normal',new_data(j,:),averages(i,:), stddevs(i,:));
+       posterior(i) = prior*prod(likelihood);
+   end
+   [max_posterior, class_ind] = max(posterior);
+   test_label(end+1) = alluniquelabels(class_ind);
+end
+new_accu =0;
+
+for  i=1:test_data_size
+    if new_label(i) == test_label(i);
+        new_accu = new_accu+1;
+    end
+end
+new_label=[];
+for j=1:train_data_size
+   for i=1:length(alluniquelabels)
+       prior = priors(i);
+       likelihood = pdf('normal',train_data(j,:),averages(i,:), stddevs(i,:));
+       posterior(i) = prior*prod(likelihood);
+   end
+   [max_posterior, class_ind] = max(posterior);
+   test_label(end+1) = alluniquelabels(class_ind);
+end
+
+train_accu =0;
+for  i=1:train_data_size
+    if test_label(i) == train_label(i);
+        train_accu = train_accu+1;
+    end
+end
+
+new_accu = new_accu/test_data_size;
+train_accu = train_accu/train_data_size;
+
+end
