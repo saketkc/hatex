81f31a376463b430f484cd738566e5cad9e81049
Saket Choudhary <saketkc@gmail.com>
2015-09-21 21:42:50 -0700

    matlab still sucks

--- 2015_Fall/CSCI-567/Homework1/data/programming/knn_classify.m
+++ 2015_Fall/CSCI-567/Homework1/data/programming/knn_classify.m
@@ -1,4 +1,34 @@
 function [new_accu, train_accu] = knn_classify(train_data, train_label, new_data, new_label, K)
+new_data_size = length(new_label);
+train_data_size = length(train_label);
+% Standardise
+
+mean_train_data = mean(train_data)
+std_train_data = mean(train_data)
+
+standardise_new_data = bsxfun(@minus, new_data, mean_train_data );
+standardise_new_data = bsxfun(@rdivide, standardise_new_data, std_train_data);
+%(new_data-mean_train_data)./std_train_data
+%standardise_train_data = (new_data-mean_train_data)./std_train_data
+
+standardise_train_data = bsxfun(@minus, new_data, mean_train_data );
+standardise_train_data = bsxfun(@rdivide, standardise_train_data, std_train_data);
+
+test_label = []
+all_distances = {}
+for i =1:new_data_size
+    distances = []
+    sample = standardise_new_data(i)
+    for j=1:train_data_size
+        distances(end+1) = norm(sample-standardise_train_data(j))
+    end
+    all_distances{i} = distances
+end
+        
+new_accu = 1
+train_accu = 1
+end
+
 % k-nearest neighbor classifier
 % Input:
 %  train_data: N*D matrix, each row as a sample and each column as a
