eeeb9f59258dbf28fda0c839459728883c076d98
Saket Choudhary <saketkc@gmail.com>
2015-10-03 17:17:50 -0700

    Add plot

--- 2015_Fall/CSCI-567/Homework2/data/run_problem_4_f.m
+++ 2015_Fall/CSCI-567/Homework2/data/run_problem_4_f.m
@@ -2,26 +2,42 @@ run_problem_4_e;
 selected_features = [];
 features = 1:size(X_train_master_pruned_normalised,2);
 features_array = [];
+training_accuracy_tracker = [];
+test_accuracy_tracker = [];
+test_label = [];
 for j=1:length(survival_train)
     train_label(j) = str2num(survival_train(j));
 end
+for j=1:length(survival_test)
+    test_label(j) = str2num(survival_test(j));
+end
+
 for j=1:10
     selected = 0;
     max_accu = 0;
     for i=1:length(features)
         %Check if this feature was not selected already
         if sum(i==selected_features)==0
-            temp_features = [features_array X_train_master_pruned(:,i)];
+            temp_features = [features_array X_train_master_pruned_normalised(:,i)];
             [accu] = glmfit_accu(temp_features, train_label');
             if accu>max_accu
                 max_accu = accu;
-                selected = i;
-                
+                selected = i;                
             end
         end
     end
-    selected_features(end+1) = selected;
-    features_array(:,end+1) = X_train_master_pruned(:, selected);
+    training_accuracy_tracker(end+1) = max_accu;
+    selected_features(end+1) = selected;    
+    test_accu = glmfit_accu(X_test_master_pruned_normalised(:,selected_features), test_label'); 
+    test_accuracy_tracker(end+1) = test_accu;
+    features_array(:,end+1) = X_train_master_pruned_normalised(:, selected);
 end
+
+plot(1:10, test_accuracy_tracker, '-r', 1:10, training_accuracy_tracker, '-g');
+title('Training/Test accuracy v/s iteration number');
+xlabel('Iteration');
+ylabel('Accuracy');
+legend('Testing accuracy', 'Training_accuracy');
+print('accuracy.png', '-dpng')
 
             
