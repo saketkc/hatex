f3efb3e9b4a53937e627f2080cb31004af226c2e
Saket Choudhary <saketkc@gmail.com>
2015-10-05 17:26:58 -0700

    Fix

--- 2015_Fall/CSCI-567/Homework2/data/CSCI567_hw2_fall15.m
+++ 2015_Fall/CSCI-567/Homework2/data/CSCI567_hw2_fall15.m
@@ -1,34 +1,34 @@
 function [] = CSCI567_hw2_fall15()
 disp('****************** Problem 4(a) Start ******************');
-run_problem_4_1_a
+run_problem_4_a
 disp('****************** Problem 4(a) End ******************');
 
 disp('****************** Problem 4(b) Start ******************');
-run_problem_4_1_b
+run_problem_4_b
 disp('****************** Problem 4(b) End ******************');
 
 disp('****************** Problem 4(c) Start ******************');
-run_problem_4_1_c
+run_problem_4_c
 disp('****************** Problem 4(c) End ******************');
 
 disp('****************** Problem 4(d) Start ******************');
-run_problem_4_1_d
+run_problem_4_d
 disp('****************** Problem 4(d) End ******************');
 
 disp('****************** Problem 4(e) Start ******************');
-run_problem_4_1_e
+run_problem_4_e
 disp('****************** Problem 4(e) End ******************');
 
 disp('****************** Problem 4(f) Start ******************');
-run_problem_4_1_f
+run_problem_4_f
 disp('****************** Problem 4(f) End ******************');
 
 disp('****************** Problem 4(g) Start ******************');
-run_problem_4_1_g
+run_problem_4_g
 disp('****************** Problem 4(g) End ******************');
 
 disp('****************** Problem 4(h) Start ******************');
-run_problem_4_1_h
+run_problem_4_h
 disp('****************** Problem 4(h) End ******************');
 
 
