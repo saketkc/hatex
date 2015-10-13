8087e274d77398998031c960c6dd548986a906c0
Saket Choudhary <saketkc@gmail.com>
2015-10-13 07:46:27 -0700

    Add

--- 2015_Fall/CSCI-567/Homework3/programming/estimate_beta.m
+++ 2015_Fall/CSCI-567/Homework3/programming/estimate_beta.m
@@ -1,6 +1,14 @@
-function [beta] = estimate_beta(n,x,y)
+function [beta] = estimate_beta(p,x,y)
 X = [];
 beta = [];
-rows_X = size(X,1);
-powers_X = 
+%% x : Column vector
+rows_X = size(x,1);
+cols_X = size(x,2);
+powers_X = 0:p-1;
+for i=1:rows_X
+    X(end+1,:) = power(x(i),powers_X);
+end
+%size(X)
+%size(y)
+beta = pinv(X'*X)*X'*y;
 end
--- 2015_Fall/CSCI-567/Homework3/programming/run_problem_5_a.m
+++ 2015_Fall/CSCI-567/Homework3/programming/run_problem_5_a.m
@@ -1,3 +1,16 @@
 clear all;
+rng(1);
 [X,Y] = problem_5_a(100);
+rows_X = size(X,1);
+beta_matrix = [];
+beta_0 = [];
+beta_1 = [];
+beta_2 = [];
+beta_3 = [];
+beta_4 = [];
+beta_5 = [];
+beta_6 = [];
 
+for i=1:rows_X
+    beta_1(end+1,:) = estimate_beta(1, X(i,:),Y(i,:)');
+end
