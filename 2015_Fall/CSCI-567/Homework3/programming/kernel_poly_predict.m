5c30df01ff7d4af8054141fcd89067e5d94dc3e5
Saket Choudhary <saketkc@gmail.com>
2015-10-14 23:16:04 -0700

    F

--- 2015_Fall/CSCI-567/Homework3/programming/kernel_poly_predict.m
+++ 2015_Fall/CSCI-567/Homework3/programming/kernel_poly_predict.m
@@ -1,10 +1,10 @@
-function [ypred] = kernel_poly(l,a,b,X,y,Xtest)
+function [ypred] = kernel_poly_predict(l,a,b,X,y,Xtest)
 n = size(X,1);
 d = size(X,2);
 K = X*X'+a;
 K = K.^b;
 cols_X = size(X,2);
-yy = X*Xtest'+a;
-yy = yy.^b;
+yy = X*Xtest';%+a;
+%yy = yy.^b;
 ypred = y'*pinv(K+l*eye(n))*yy;
 end
