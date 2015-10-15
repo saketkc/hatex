f5198f36f4887ae299507b8ead2429873dea862d
Saket Choudhary <saketkc@gmail.com>
2015-10-14 18:47:40 -0700

    Fin

--- 2015_Fall/CSCI-567/Homework3/programming/perform_k_fold_validation_poly.m
+++ 2015_Fall/CSCI-567/Homework3/programming/perform_k_fold_validation_poly.m
@@ -1,4 +1,4 @@
-function [err, W] = perform_k_fold_validation_poly(a,b, input_data)
+function [err, W] = perform_k_fold_validation_poly(lambda, a,b, input_data)
     k = 5;
     cols = size(input_data,2);
     datasize = size(input_data,1);
@@ -18,8 +18,8 @@ function [err, W] = perform_k_fold_validation_poly(a,b, input_data)
             end
         end
         test_data = input_data(training_dataset_indices(index,:),:);
-        [ypred] = kernel_poly(a,b,training_data(:,2:cols), training_data(:,1));
-        [ypredtest] = kernel_poly_predict(a,b,training_data(:,2:cols), training_data(:,1), training_data(:,2:cols));%, training_data(:,1));
+        [ypred] = kernel_poly(lambda,a,b,training_data(:,2:cols), training_data(:,1));
+        [ypredtest] = kernel_poly_predict(lambda, a,b,training_data(:,2:cols), training_data(:,1), test_data(:,2:cols));%, training_data(:,1));
         [yerror] = kernel_poly_estimate_error(ypredtest, test_data(:,1));
         err = err+yerror;
     end
--- 2015_Fall/CSCI-567/Homework3/programming/run_problem_6_c.m
+++ 2015_Fall/CSCI-567/Homework3/programming/run_problem_6_c.m
@@ -5,18 +5,20 @@ datasize = size(data,1);
 indices = 1:datasize;
 training_data_indices = [];
 test_data_indices = [];
-sigma = [0.125, 0.25, 0.5, 1, 2, 4, 8];
+a = [−1, −0.5, 0, 0.5, 1];
+c = [1, 2, 3, 4];
 lambda = [0 power(10,-4:3)];
 train_err =[];
 test_err = [];
 optimal_sigmas=[];
 optimal_lambdas=[];
-
+optimal_a=[];
+optimal_b=[];
 for t=1:10
     minmodelerror = 100;
-    optimalsigma = 0;
-    optimalW = 0;
     optimallamb = 0;
+    optimala = 0;
+    optimalb = 0;
     permutation = randperm(datasize);
     train_ind = permutation(1:floor(0.8*datasize));
     test_ind = permutation (ceil(0.8*datasize):length(permutation));
@@ -42,23 +44,27 @@ for t=1:10
         sigm = sigma(l);
         for k=1:length(lambda)
             lamb = lambda(k);
-            [modelerror,W] = perform_k_fold_validation_rbf(sigm, lamb, norm_train_data_appended);
+            [modelerror,W] = perform_k_fold_validation_poly(sigm, lamb, norm_train_data_appended);
             modelerror
             if modelerror < minmodelerror
                 minmodelerror = modelerror;
                 optimalW = W;
-                optimalsigma = sigm;
+                optimala = a;
+                optimalb = b;
                 optimallamb = lamb;
             end
             optimalsigma ;
         end
     end
     cols = size(norm_train_data_appended,2);
-    newmodelw = kernel_rbf(optimalsigma, optimallamb, norm_train_data_appended(:,2:cols), norm_train_data_appended(:,1));
-    testerror = kernel_linear_estimate_error(newmodelw,norm_test_data_appended(:,2:size(norm_test_data_appended,2)),norm_test_data_appended(:,1));
+    %ymodelerr = kernel_poly(optimallamb, optimala, optimalb, norm_train_data_appended(:,2:cols), norm_train_data_appended(:,1));  
+    %testerror = kernel_linear_estimate_error(newmodelw,norm_test_data_appended(:,2:size(norm_test_data_appended,2)),norm_test_data_appended(:,1));
+    [ypredtest] = kernel_poly_predict(optimallambda, a,b, norm_train_data_appended(:,2:cols), norm_train_data_appended(:,1), norm_test_data_appended(:,2:cols));%, training_data(:,1));
+    [testerror] = kernel_poly_estimate_error(ypredtest, norm_train_data_appended(:,1));
     test_err(end+1) = testerror;
     optimal_lambdas(end+1) = optimallamb;
-    optimal_sigmas(end+1) = optimalsigma;
+    optimal_a(end+1) = optimala;
+    optimal_b(end+1) = optimalb;
 
 end
 
