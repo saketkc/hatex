5cc5574a2f347eb48ac2dca98bcba66aaeb35f90
Saket Choudhary <saketkc@gmail.com>
2015-10-16 16:44:55 -0700

    add

--- 2015_Fall/CSCI-567/Homework3/programming/run_problem_6_a.m
+++ 2015_Fall/CSCI-567/Homework3/programming/run_problem_6_a.m
@@ -13,27 +13,7 @@ for t=1:10
     minmodelerror = 100;
     optimallambda = 0;
     optimalW = 0;
-    permutation = randperm(datasize);
-    train_ind = permutation(1:floor(0.8*datasize));
-    test_ind = permutation (ceil(0.8*datasize):length(permutation));
-    training_data_indices(end+1,:) = train_ind;
-    test_data_indices(end+1,:) = test_ind;
-    train_data = data(train_ind,:);
-    test_data  = data(test_ind,:);
-    [norm_train_data, mean_train_data, std_train_data] = normalise(train_data);
-    norm_test_data = normalise_with_mean(test_data, mean_train_data, std_train_data);
-    norm_train_data(:,1) = train_data(:,1);
-    norm_test_data(:,1) = test_data(:,1);
-    norm_test_data_appended = norm_test_data(:,1);
-    norm_test_data_appended(:,2) = ones(size(norm_test_data,1),1);
-    norm_test_data_appended(:,3:size(norm_test_data,2)+1) = norm_test_data(:,2:size(norm_test_data,2));
-
-
-
-    norm_train_data_appended = norm_train_data(:,1);
-    norm_train_data_appended(:,2) = ones(size(norm_train_data,1),1);
-    norm_train_data_appended(:,3:size(norm_train_data,2)+1) = norm_train_data(:,2:size(norm_train_data,2));
-    
+   [norm_train_data_appeneded, norm_test_data_appeneded] = getdata(data);
 
     for l=1:length(lambda)
         lamb = lambda(l);
