73b485204ab3d5f8f711275bd8956767dfffd4e9
Saket Choudhary <saketkc@gmail.com>
2015-11-15 10:12:36 -0800

    Dump

---
+++ 2015_Fall/CSCI-567/Homework5/run_problem_42.m
@@ -0,0 +1,24 @@
+clear all;
+rng(1);
+data = load('hw5_blob.mat');
+blob = data.points;
+
+data = load('hw5_circle.mat');
+circle = data.points;
+
+K = [2, 3, 5];
+
+for i=1:numel(K)
+    k = K(i);
+    clusters = kmeans(blob,k);
+    scatter(blob(:,1), blob(:,2), [], clusters, 'filled');
+    print(sprintf('blob-%d',k), '-dpng');
+end
+
+
+for i=1:numel(K)
+    k = K(i);
+    clusters = kmeans(circle,k);
+    scatter(circle(:,1), circle(:,2), [], clusters, 'filled');
+    print(sprintf('blob-%d',k), '-dpng');
+end
