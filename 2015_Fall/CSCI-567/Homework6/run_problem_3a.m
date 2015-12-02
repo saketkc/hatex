cc49a2a168dbfdc49f91f55d8c6614ccfd426499
Saket Choudhary <saketkc@gmail.com>
2015-12-02 13:01:04 -0800

    d

--- 2015_Fall/CSCI-567/Homework6/run_problem_3a.m
+++ 2015_Fall/CSCI-567/Homework6/run_problem_3a.m
@@ -2,6 +2,6 @@ load('hw6_pca.mat')
 eigenvecs = get_sorted_eigenvecs(X.train);
 [N,D] = size(X.train);
 for i=1:8
-    im = double(reshape(eigenvecs(:,i), 16, 16));
+    im = 255*double(reshape(eigenvecs(:,i), 16, 16));
     imwrite(im, sprintf('eig-%d.png', i));
 end
