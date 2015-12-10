34109fbf2c39ecc44a0963495a0eef9b39ca55c8
Saket Choudhary <saketkc@gmail.com>
2015-12-09 18:32:10 -0800

    And done

---
+++ 2015_Fall/MATH-541B/pca/pca_part_c.m
@@ -0,0 +1,33 @@
+load('PCA_Exercise_Images.mat')
+vectorized_images = [];
+[nrow, ncol, nimg] = size(Photo_Images);
+original = double(Photo_Images(:,:,70));
+for i=1:30
+    for j=1:30
+        patch = original(20*(i-1)+1:20*(i), 20*(j-1)+1:20*j);
+        vimg = double(vectorize_image(patch));
+        vectorized_images = [vectorized_images; vimg];
+    end
+end
+[eigvals, eigvecs, projected, reconstructed] = perform_pca(vectorized_images, 10);
+
+%imagesc(original);
+%print('pca-part-c-original', '-dpng');
+%close all;
+
+rec_patches_all = [];
+rec_patches_temp = [];
+for i=1:900
+    rec_patch = reshape(reconstructed(i, :),20,20);
+    rec_patches_temp = [rec_patches_temp  rec_patch];
+    if (mod(i,30)==0)
+        %rest
+        rec_patches_all= [rec_patches_all;rec_patches_temp];
+        rec_patches_temp = [];
+
+    end
+end
+%imagesc(reshape(reconstructed,600,600));
+imagesc(rec_patches_all);
+print('pca-part-c-reconstructed', '-dpng');
+close all;
