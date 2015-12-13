2758350081ca433e1ba9e90b70fc2574c357eab3
Saket Choudhary <saketkc@gmail.com>
2015-12-13 15:59:11 -0800

    add

--- 2015_Fall/MATH-541B/kalman/code/utils_rotations/express_vector_in_quat_frame.m
+++
@@ -1,6 +0,0 @@
-function vout = express_vector_in_quat_frame(vin, q)
-
-     % Authors: Pieter Abbeel (pabbeel@cs.berkeley.edu)
-     %          Adam Coates (acoates@cs.stanford.edu)
-
-vout = rotate_vector(vin, [-q(1:3); q(4)]);
