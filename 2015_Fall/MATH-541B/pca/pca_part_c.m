b03a20400f0e2da62bb6562988ae551b11009223
Saket Choudhary <saketkc@gmail.com>
2015-12-14 18:57:52 -0800

    Add

--- 2015_Fall/MATH-541B/pca/pca_part_c.m
+++ 2015_Fall/MATH-541B/pca/pca_part_c.m
@@ -1,9 +1,23 @@
 load('PCA_Exercise_Images.mat')
 vectorized_images = [];
-img = VTEC_Images;
-index = 170;
+images = {};
+images{1} = Photo_Images;
+images{2} = VTEC_Images;
+imgindex = 1;
+img = images{imgindex}; 
+title = 'VTEC_Images';
+index = 170;    
+
+if (imgindex ==1)
+    title = 'Photo_Images';
+    index = 70;    
+end
+
 [nrow, ncol, nimg] = size(img);
 original = double(img(:,:,index));
+
+
+
 r = 80;
 for i=1:nrow/20
     for j=1:ncol/20
@@ -12,6 +26,7 @@ for i=1:nrow/20
         vectorized_images = [vectorized_images; vimg];
     end
 end
+mean_patch = mean(vectorized_images);
 [eigvals, eigvecs, projected, reconstructed] = perform_pca(vectorized_images, r);
 
 imagesc(original);
@@ -34,3 +49,13 @@ end
 imagesc(rec_patches_all);
 print(sprintf('pca-part-c-vtec-reconstructed-r=%d',r), '-dpng');
 close all;
+
+
+all_patch_minus_mean = bsxfun(@minus, vectorized_images, mean_patch);
+all_patch_minus_mean = all_patch_minus_mean.^2;
+euc_distance = sum(sqrt(sum(all_patch_minus_mean,2)))/(size(vectorized_images,1));
+
+
+new_distance = bsxfun(@minus, reconstructed, mean_patch);
+new_distance = new_distance.^2;
+new_euc_distance = sum(sqrt(sum(new_distance,2)))/(size(vectorized_images,1));
