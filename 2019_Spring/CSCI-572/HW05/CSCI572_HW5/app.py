5ae0a4e8a73eefe9f2e5cb94db72337f1d3181f1
Saket Choudhary <saketkc@gmail.com>
2019-04-22 10:57:18 -0700

    Updates

--- 2019_Spring/CSCI-572/HW05/CSCI572_HW5/app.py
+++ 2019_Spring/CSCI-572/HW05/CSCI572_HW5/app.py
@@ -1,53 +1,33 @@
 from flask import Flask, Response, render_template, request
 import json
 from wtforms import TextField, Form
-import solr
-import pandas as pd
-from spell import candidates, correction
+from spell import candidates  # , correction
 
 app = Flask(__name__)
 
-cities = ["Bratislava",
-          "Banská Bystrica",
-          "Prešov",
-          "Považská Bystrica",
-          "Žilina",
-          "Košice",
-          "Ružomberok",
-          "Zvolen",
-          "Poprad"]
-
 
 class SearchForm(Form):
-    autocomp = TextField('Search: ', 
-                         id='autocomplete')
+    autocomp = TextField("Search: ", id="autocomplete")
 
 
-@app.route('/_autocomplete', methods=['GET', 'POST'])
+@app.route("/_autocomplete", methods=["GET", "POST"])
 def autocomplete():
-    if request.method=='POST':
-        #print(request.term)
-        #print(request.get_json())
+    if request.method == "POST":
         data = str(request.get_data())
-        search_term = data.split('=')[1]
-        #search_term = request.args.get('term')
-        print(data)
-                   
-    #search_string = str(request.args.get('input'))
-    #print(search_string)
-    #print(search_term)
+        search_term = data.split("=")[1]
+
     if search_term:
         options = candidates(search_term)
-        print(options)
     else:
         options = []
-    return Response(json.dumps(options), mimetype='application/json')
+    return Response(json.dumps(options), mimetype="application/json")
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route("/", methods=["GET", "POST"])
 def index():
     form = SearchForm(request.form)
     return render_template("search.html", form=form)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
